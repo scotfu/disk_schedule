74930936a7dad5f1b5d260ccc80abe751c172c8e
scotfu <scotfu@gmail.com>
2013-04-08 21:04:17 -0400

    fix bugs

--- disk_schedule.py
+++ disk_schedule.py
@@ -4,32 +4,33 @@ import random
 def get_SCAN_direction(current,input):
     '''
     Decide the initial direction the SACN choose,up or down.
-    Compare the current location with the first visited location,
-    if they are the same, compared the current with the next of the list.
-    It is impossible that this function runs more than two times.
+    Comparing the current track with the first should visited track of
+    the input list,if two track numbers are the same, compared the current track
+    with the next track.It is impossible that this function runs more than
+    two times,because the input is a set of number.
     '''
     if current-input[0] > 0:
-        direction = u'down'
+        return u'down'
     elif current-input[0] < 0:
-        direction = u'up'
+        return  u'up'
     else:
-        get_SCAN_direction(current,input[1:])
-    return direction    
+        # current == input[0]
+        return get_SCAN_direction(current,input[1:])
+
 
 def SCAN(current,input):
     '''
-    SACN algorithm act as a elevator,continue to travel in current direction
+    SACN algorithm acts as a elevator,continue to travel in current direction
     until the end, then go the opposite direction till the ohter end.
     '''
     direction = get_SCAN_direction(current,input)
-    first = input[0]
     input.append(current)
     input.sort()
     if current == input[-1]:
+        #current is the end of the list, so just go down all the way
         path_cost = current-input[0]
         visited_list = list(reversed(input))
     elif input[0] < current < input[-1]:    
-
         index = input.index(current)
         if direction == u'down':
             visited_list=list(reversed(input[0:index+1]))+input[index+1:]
@@ -38,6 +39,7 @@ def SCAN(current,input):
             visited_list=input[index:]+list(reversed(input[:index]))
             path_cost=input[-1]*2-current-input[0]
     else:
+        #current is the start of the list, so just go up all the way
         path_cost= input[-1] - current
         visited_list = input
     return path_cost,visited_list
@@ -45,19 +47,20 @@ def SCAN(current,input):
 
 def SSF(current,input):
     '''
-    Shortest seek first, keep comparing the two neighbours of current. The 
-    closest one goes first. When raise a IndexError, the current must be the
-    first(0) or last one(-1) of the sorted list. Then the problem is easy now,
-    travels in one direction, up or down depends on first or last. 
+    Shortest seek first, keep comparing the two neighbours of current track.
+    The closest one goes first. When raise a IndexError, the current must be the
+    first(0) or last(-1) track of the sorted list. Then the rest problem is easy
+    now, travels in one direction, up or down depends on first or last. 
     '''
     visited_list=[]
     path_cost = 0
     input.append(current)
-    input.sort()#add the current to the input and sort the input
+    input.sort()#add the current to the input list and sort the input
     try:
         while input:
             index=input.index(current)#current location
-            if input[index-1]-current >= input[index+1]-current:
+            if current-input[index-1] >= input[index+1]-current:
+                #comparing
                 path_cost += (input[index+1]-current)
                 visited_list.append(current)
                 current = input[index+1]#next current
@@ -77,6 +80,11 @@ def SSF(current,input):
     return path_cost,visited_list
 
 def generate_data():
+    '''
+    Generate a set of number, each number in the input list
+    stands for a request for the track of that number.Head is 
+    in the current track at the beginning. 
+    '''
     input=[]
     count=0
     while count < 20:
@@ -89,9 +97,9 @@ def generate_data():
 
 if __name__ == '__main__':
     input,current = generate_data()
-    input=[100, 50, 10, 20, 75]
-    current=35
-    print 'input:%s\ncurrent:%s'%(input,current)
-    print 'SCAN\npathcost:%s\nvisited order:%s'%SCAN(current,input[:])
-    print 'SSF\npathcost:%s\nvisited order:%s'%SSF(current,input[:])
+    input = [100, 50, 10, 20, 75]
+    current = 100
+    print 'input:%s\ncurrent track:%s'%(input,current)
+    print 'Using SCAN\npath cost:%s\nvisited order:%s'%SCAN(current,input[:])
+    print 'Using SSF\npath cost:%s\nvisited order:%s'%SSF(current,input[:])
     
