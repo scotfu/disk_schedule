6ed50ae17b0e78f6a5e31dc15b71b8b05b53c5b5
scotfu <scotfu@gmail.com>
2013-04-11 01:07:31 -0400

    add LOOK, change SCAN, set CYLINDER as constant

--- disk_schedule.py
+++ disk_schedule.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8 -*-
 import random
+CYLINDER_START = 1
+CYLINDER_END = 1024
 
 def get_SCAN_direction(current,input):
     '''
@@ -17,11 +19,39 @@ def get_SCAN_direction(current,input):
         # current == input[0]
         return get_SCAN_direction(current,input[1:])
 
-
 def SCAN(current,input):
     '''
-    SACN algorithm acts as a elevator,continue to travel in current direction
+    SACN algorithm acts like a elevator, continue to travel in current direction
     until the end, then go the opposite direction till the other end.
+    '''
+    direction = get_SCAN_direction(current,input)
+    input.append(current)
+    input.sort()
+    if current == input[-1]:
+        #current is the end of the list, so just go down all the way
+        path_cost = current- CYLINDER_START
+        visited_list = list(reversed(input))
+    elif input[0] < current < input[-1]:    
+        index = input.index(current)
+        if direction == u'down':
+            #first from current go down to the start of the input, 
+            #then go up to the end
+            visited_list=list(reversed(input[0:index+1]))+input[index+1:]
+            path_cost = current + CYLINDER_END - CYLINDER_START*2
+        else:
+            visited_list = input[index:] + list(reversed(input[:index]))
+            path_cost = CYLINDER_END*2 - current - CYLINDER_START
+    else:
+        #current is the start of the list, so just go up all the way
+        path_cost = CYLINDER_END - CYLINDER_START
+        visited_list = input
+    return path_cost,visited_list
+
+
+def LOOK(current,input):
+    '''
+    LOOK is similar to SCAN,but LOOK will change directions when the head has
+    reached the last request in the current direction, acts as a elevator.
     '''
     direction = get_SCAN_direction(current,input)
     input.append(current)
@@ -109,6 +139,19 @@ def FIFO(current,input):
         path_cost += abs(visited_list[n]-visited_list[n+1])
     return path_cost, visited_list
 
+def RSS(current,input):
+    '''
+    Random scheduling
+    '''
+    path_cost = 0
+    visited_list=[current]
+    while input:
+        next = random.choice(input)
+        input.remove(next)
+        visited_list.append(next)
+        path_cost += abs(current - next)
+        current = next
+    return path_cost, visited_list
 
 def generate_data():
     '''
@@ -119,20 +162,21 @@ def generate_data():
     input=[]
     count=0
     while count < 20:
-        input.append(random.randint(1,65536))
+        input.append(random.randint(CYLINDER_START,CYLINDER_END))
         count+=1
     input=list(set(input))    
-    current = random.randint(1,65536)
+    current = random.randint(CYLINDER_START,CYLINDER_END)
     return input,current
 
 
 if __name__ == '__main__':
     input,current = generate_data()
-    input = [39105, 47330, 31195, 39888, 10853, 48810, 59627, 38421, 48750, 10896, 51251, 16565, 63254, 45139, 19479, 18683, 16092, 54109, 9118, 31263]
-    current = 14651
+    input=[1,2,334,454,676,3,434,78,899,232,343,454,782,45,13,1024]
     print 'input:%s\ncurrent track:%s'%(input,current)
-#    print 'Using SCAN\npath cost:%s\nvisited order:%s'%SCAN(current,input[:])
-#    print 'Using CSCAN\npath cost:%s\nvisited order:%s'%CSCAN(current,input[:])
+    print 'Using SCAN\npath cost:%s\nvisited order:%s'%SCAN(current,input[:])
+    print 'Using LOOK\npath cost:%s\nvisited order:%s'%LOOK(current,input[:])
+    print 'Using CSCAN\npath cost:%s\nvisited order:%s'%CSCAN(current,input[:])
     print 'Using SSF\npath cost:%s\nvisited order:%s'%SSF(current,input[:])
-#    print 'Using FIFO\npath cost:%s\nvisited order:%s'%FIFO(current,input[:])
+    print 'Using FIFO\npath cost:%s\nvisited order:%s'%FIFO(current,input[:])
+    print 'Using RSS\npath cost:%s\nvisited order:%s'%RSS(current,input[:])
     
